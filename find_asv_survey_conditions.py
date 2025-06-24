4250956dc3d5cb33dd0c6a9b12a2b81c7759e06c
Kara Koetje <kkoetje@gmail.com>
2025-06-24 16:37:12 +0000

    Update date lists; update marker colors; make new plots.

--- find_asv_survey_conditions.py
+++ find_asv_survey_conditions.py
@@ -5,9 +5,9 @@ from matplotlib import pyplot as plt
 
 ############### USER INPUTS ###############
 
-ofname = 'all_jb_wave_conditions.png'  # name of figure to be printed
-#yf_date_file_name = 'all_yellowfin_dates.txt'  # Comment this line out if you don't want to include these dates
-jb_date_file_name = 'all_jaiabot_dates.txt'  # Comment this line out if you don't want to include these dates
+ofname = 'all_yf_wave_conditions.png'  # name of figure to be printed
+yf_date_file_name = 'all_yellowfin_dates.txt'  # Comment this line out if you don't want to include these dates
+#jb_date_file_name = 'all_jaiabot_dates.txt'  # Comment this line out if you don't want to include these dates
 sampling_hours = 1  # number of hours for each survey, assuming start time below (one datapoint will be plotted for each hour)
 start_hour = 13  # assumed start hour in UTC (ie 1300 UTC)
 
@@ -58,22 +58,24 @@ def conditions_operated_plot(ofname, all_waves, bins, tp_mean, tp_std,
     # Panel 1: Vertical lines for survey dates
     ax1 = plt.subplot2grid((3, 2), (0, 0), colspan=2)
     ax1.set_title('FRF - 17m Waverider Wave Height')
-    ax1.plot(all_waves['time'], all_waves['Hs'], label='Wave Height') #, color='#5A5A5A')
+    ax1.plot(all_waves['time'], all_waves['Hs'], label='Wave Height', color='#4d4d4d')
     ax1.set_ylabel('Wave Height [m]')
     ax1.set_xlabel('Date')
 
     if yf_dates_list:
         for d in yf_dates_list:
-            ax1.axvline(dt.datetime.strptime(d, '%Y%m%d'), color='#FFDB58', linestyle='--', linewidth=1,
-                        label='Yellowfin')
+            ax1.axvline(dt.datetime.strptime(d, '%Y%m%d'), color='#ff7f0e', linestyle='--', linewidth=1,
+                        label='Yellowfin')  #color='#FFDB58'
 
     if jb_dates_list:
         for d in jb_dates_list:
-            ax1.axvline(dt.datetime.strptime(d, '%Y%m%d'), color='red', linestyle='--', linewidth=1, label='JaiaBot')
+            ax1.axvline(dt.datetime.strptime(d, '%Y%m%d'), color='#1f77b4', linestyle='--', linewidth=1, label='JaiaBot')
 
     # Panel 2: Wave conditions during surveys
     ax2 = plt.subplot2grid((3, 2), (1, 0), colspan=2, rowspan=2)
     ax2.set_title('Wave Conditions During Collections')
+    ax2.fill_between(bins, tp_mean + tp_std, tp_mean - tp_std, alpha=0.25, color='black', label='67%')
+    ax2.fill_between(bins, tp_mean + 2 * tp_std, tp_mean - 2 * tp_std, alpha=0.25, color='black', label='95%')
 
     if yf_dates_list:
         yf_dates = [dt.datetime.strptime(date, '%Y%m%d') for date in yf_dates_list]
@@ -84,7 +86,7 @@ def conditions_operated_plot(ofname, all_waves, bins, tp_mean, tp_std,
             mask = np.isin(all_wave_dates, yf_times)
             yf_hs.extend(all_waves['Hs'][mask])
             yf_tp.extend(1 / all_waves['peakf'][mask])
-        ax2.scatter(yf_hs, yf_tp, marker='D', c='#FFDB58', s=50, edgecolor='k', label='Yellowfin')
+        ax2.scatter(yf_hs, yf_tp, marker='D', c='#ff7f0e', s=50, edgecolor='k', label='Yellowfin')  #c='#FFDB58'
 
     if jb_dates_list:
         jb_dates = [dt.datetime.strptime(date, '%Y%m%d') for date in jb_dates_list]
@@ -95,10 +97,8 @@ def conditions_operated_plot(ofname, all_waves, bins, tp_mean, tp_std,
             mask = np.isin(all_wave_dates, jb_times)
             jb_hs.extend(all_waves['Hs'][mask])
             jb_tp.extend(1 / all_waves['peakf'][mask])
-        ax2.scatter(jb_hs, jb_tp, marker='o', c='red', s=50, edgecolor='k', label='JaiaBot')
+        ax2.scatter(jb_hs, jb_tp, marker='o', c='#1f77b4', s=50, edgecolor='k', label='JaiaBot')
 
-    ax2.fill_between(bins, tp_mean + tp_std, tp_mean - tp_std, alpha=0.25, color='black', label='67%')
-    ax2.fill_between(bins, tp_mean + 2 * tp_std, tp_mean - 2 * tp_std, alpha=0.25, color='black', label='95%')
     ax2.set_xlabel('Wave Height [m]')
     ax2.set_ylabel('Wave Period [s]')
     ax2.set_xlim([0.25, 2])
